f76f74bfa9f9ae3ab63b0a08133418431fe06599
shakileash <shakileash2000@gmail.com>
2020-12-29 22:46:15 +0530

    adding files

--- App.js
+++ App.js
@@ -1,11 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { StyleSheet, Text, View,TextInput,ScrollView,TouchableOpacity,Image} from 'react-native';
+import {Provider as PaperProvider ,IconButton, Colors,RadioButton,Searchbar,Appbar   } from 'react-native-paper';
+import Constants from "expo-constants";
+import { Icon } from 'react-native-elements'
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View elevation={15} style={styles.header}> 
+        <Text style={{alignSelf:"flex-start",color:"#E6EFF9",fontWeight:"bold",fontSize:18,}}>CryptoCurrencies</Text>
+        <TouchableOpacity style={{marginLeft:10,marginTop:5,}}>
+          <LinearGradient start={[0, 0.5]}
+                          end={[1, 0.5]}
+                          colors={['#0D324D', '#7F5A83']}
+                          style={{borderRadius: 5,justifyContent:"center",width:40,height:20,alignItems:"center"}}>
+            <View >
+              <Text style={{alignSelf:"baseline",color:"#E6EFF9",fontWeight:"bold",fontSize:12,}}>INR</Text>
+            </View>
+          </LinearGradient>
+        </TouchableOpacity>
+        <IconButton
+        icon="magnify"
+        color={"#27D7CD"}
+        style={{alignSelf:"flex-end",right:0,position:"absolute",top:0}}
+        >
+        </IconButton> 
+      </View>
+
+      <ScrollView style={styles.cardView} > 
+        <View style={{flexDirection:"row",backgroundColor:"#343C5F",padding:10,width:'100%',borderRadius:15,height:"100%"}}>
+          
+          
+          <View style={{flexDirection:"column",width:"100%"}}>
+            <View style={{flex:1,flexDirection:"row"}}>
+                <Image source={{ uri:"https://raw.githubusercontent.com/condacore/cryptocurrency-icons/master/128x128/bitcoin.png"}} style={styles.profileImg} />
+                <View style={{flexDirection:"column",justifyContent:"center",marginLeft:15,margin:10}}>
+                  <Text style={{alignSelf:"flex-start",color:"white",fontWeight:"bold",fontSize:13,}} >Bitcoin</Text>
+                  <Text style={{alignSelf:"flex-start",color:"grey",fontSize:11,}} >BTC</Text>
+                </View>
+                <Text style={{alignSelf:"flex-start",color:"white",fontSize:10,fontWeight:"200",marginLeft:15,marginTop:14}}>Current Price </Text>
+                < Icon name="caret-up" color="#49AF41" type="font-awesome" style={{marginLeft:10,marginTop:8}} />
+                <Text style={{alignSelf:"flex-start",color:"#63F763",fontSize:15,fontWeight:"bold",marginLeft:5,marginTop:10}}>199822.0</Text>
+                < Icon solid name="crosshairs" color="yellow" type="font-awesome" style={{marginLeft:10,marginTop:8}} />
+
+          </View>
+          <View
+              style={{
+                borderBottomWidth: 1,
+                borderBottomColor: 'grey',
+                width:"100%"
+              }}
+            />
+          <View style={{flexDirection:"column",justifyContent:"center",alignItems:"center",marginLeft:15,}}>
+            <View style={{flexDirection:"row",alignItems:"center",justifyContent:"center"}}>
+              
+              <Text style={{alignSelf:"flex-start",color:"#F7F2F2",fontSize:12,marginTop:1}}> 1H% </Text>
+              <Text style={{alignSelf:"flex-start",color:"#ED4337",fontSize:13,fontWeight:"bold"}}>-490</Text>
+              <View
+              style={{
+                borderRightWidth: 1,
+                borderRightColor: 'grey',
+                height:"100%",
+                margin:5
+              }}
+            />
+              <Text style={{alignSelf:"flex-start",color:"white",fontSize:12,marginTop:1}}> 1D% </Text>
+              <Text style={{alignSelf:"flex-start",color:"#63F763",fontSize:13,fontWeight:"bold"}}>+290</Text>
+              <View
+              style={{
+                borderRightWidth: 1,
+                borderRightColor: 'grey',
+                height:"100%",
+                margin:5
+              }}
+            />
+              <Text>Hooked</Text>
+            </View>
+            
+          </View>
+          </View>
+        </View>
+      </ScrollView>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +91,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#141A3E',
+    paddingTop:Constants.statusBarHeight,
+    flexDirection:"column",
+    zIndex:0,
+  },
+  header:{
+    flexDirection:"row",
+    width:"100%",
+    padding:10,
+    height:25,
+    backgroundColor:"#16193E",
+    height:50,
+    borderBottomColor: '#12183A',
+    borderBottomWidth: 0.7,
+  },
+  profileImg: {
+    height: 25,
+    width: 25,
+    borderRadius: 40,
+    marginTop:15,
+  },
+  cardView: {
+    padding:20,
+    width:'100%',
+    flexDirection:"column",
+    borderColor:"grey",
+    borderRadius:5,
+    marginBottom:5,
   },
 });
